4b1ffee9971d73368b1b1264aeba861e3a809c30
Vansovski <marcelo_vansovski@hotmail.com>
2022-06-21 19:13:38 -0300

    Navegacao

--- src/app/produto-detalhes/produto-detalhes.component.ts
+++ src/app/produto-detalhes/produto-detalhes.component.ts
@@ -1,19 +1,28 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit } from "@angular/core";
 
-import { ActivatedRoute } from '@angular/router';
+import { ActivatedRoute } from "@angular/router";
 
-import { Product, products } from '../products';
+import { Product, products } from "../products";
 
 @Component({
-  selector: 'app-produto-detalhes',
-  templateUrl: './produto-detalhes.component.html',
-  styleUrls: ['./produto-detalhes.component.css']
+  selector: "app-produto-detalhes",
+  templateUrl: "./produto-detalhes.component.html",
+  styleUrls: ["./produto-detalhes.component.css"],
 })
 export class ProdutoDetalhesComponent implements OnInit {
+  produto: Product | undefined;
 
-  constructor() { }
+  //Injeçao para intergir com a rota
+  constructor(private route: ActivatedRoute) {}
 
   ngOnInit(): void {
-  }
+    //Obter Id do Produto pela Url
+    const routeParams = this.route.snapshot.paramMap;
+    const produtoId = Number(routeParams.get("produtoId"));
 
+    //Obter o produto correspondente
+    this.produto = products.find(
+      (produto) => produto.id === produtoId
+    );
+  }
 }
