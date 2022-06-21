d792e90764a08a7b2d61d4a703e0e9d25bc158df
Vansovski <marcelo_vansovski@hotmail.com>
2022-06-21 20:37:32 -0300

    FormAngular

--- src/app/carrinho/carrinho.component.ts
+++ src/app/carrinho/carrinho.component.ts
@@ -1,19 +1,35 @@
-import { Component, OnInit } from '@angular/core';
-import { CarrinhoService } from '../carrinho.service';
-import { Product } from '../products';
+import { Component, OnInit } from "@angular/core";
+import { FormBuilder } from "@angular/forms";
+import { CarrinhoService } from "../carrinho.service";
+import { Product } from "../products";
 
 @Component({
-  selector: 'app-carrinho',
-  templateUrl: './carrinho.component.html',
-  styleUrls: ['./carrinho.component.css']
+  selector: "app-carrinho",
+  templateUrl: "./carrinho.component.html",
+  styleUrls: ["./carrinho.component.css"],
 })
 export class CarrinhoComponent implements OnInit {
 
-  itens:Product[] = this.carroServico.getItens(); 
+  itens: Product[] = this.carroServico.getItens();
 
-  constructor(private carroServico:CarrinhoService) { }
+  form = this.fb.group(
+    {
+      nome:'',
+      end:''
+    }
+  );
 
-  ngOnInit(): void {
+  constructor(private carroServico: CarrinhoService, 
+              private fb: FormBuilder) {}
+
+  ngOnInit(): void {}
+
+
+  OnSubmit():void
+  {
+    //Processa o carrinho de compras 
+    this.itens = this.carroServico.esvazia();
+    console.warn('Pedido Enviado!', this.form.value);
+    this.form.reset();
   }
-
 }
